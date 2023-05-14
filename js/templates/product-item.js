7f69f3da61b7a82fa470cc584ccb2cc6b17ba8d0
Yana <yanaegorova69.3@gmail.com>
2023-05-15 00:17:56 +0300

    +

--- js/templates/product-item.js
+++ js/templates/product-item.js
@@ -2,15 +2,11 @@ export const item = product => {
   return `
     <li class="product__item item">
       <div class="product__block js_product glass glass-m" id="${product.id}">
-     
-      <div class="product__img-box">
-      <img src="${product.image}" alt="" class="product__block-img">
-      </div>
-      <div class="product__text-box">
-      <div class="product__text--inner">
-      <p class="product__name js_product-name">${product.name}</p> 
+      <div class="product__block--inner">
       <span class="product__price">$${product.price.toFixed(2)}</span>
-      </div>
+      <img src="${product.image}" alt="" class="product__block-img">
+      <div class="product__text-box">
+      <p class="product__name js_product-name">${product.name}</p> 
       ${
         product.description[0].text.length > 0
           ? '<p class="text product__desc">' +
@@ -19,7 +15,6 @@ export const item = product => {
             '</p>'
           : ''
       }
-    
       <div class="product__btn-block">
       <a href="product-page.html?id=${
         product.id
@@ -35,13 +30,8 @@ export const item = product => {
           : '<button id="btn-2" class="product__btn second js_add-to-cart">buy <svg  version="1.1"  xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" enable-background="new 0 0 512 512" xml:space="preserve"><polygon points="315.1,48.6 196.9,48.6 354.5,206.1 0,206.1 0,284.9 354.5,284.9 196.9,442.4 315.1,442.4 512,245.5 "/></svg></button>'
       }
      </div>
-     </div>
-     
- 
-
-
-
-  
+      </div>
+      </div>
      </div>
    </li>
     `;
